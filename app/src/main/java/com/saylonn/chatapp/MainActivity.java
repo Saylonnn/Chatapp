22d32ecf0d2c5e9d8799b158da4edfd9a4fd0fe9
samue <samuel.raak@gmx.de>
2023-01-06 10:32:55 +0100

    added popup login v1

--- app/src/main/java/com/saylonn/chatapp/MainActivity.java
+++ app/src/main/java/com/saylonn/chatapp/MainActivity.java
@@ -1,24 +1,38 @@
 package com.saylonn.chatapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.saylonn.chatapp.databinding.ActivityMainBinding;
+import com.saylonn.chatapp.ui.dialogs.LoginDialog;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean loggedIn = false;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+        String username = sp.getString("login_username", "empty");
+        String password = sp.getString("login_pw", "empty");
+        String fcm_token = sp.getString("fcm_token", "empty");
+
+        if (username.equals("empty") || password.equals("empty") || fcm_token.equals("empty")){
+            showLoginDialog();
+        }
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -32,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    private void showLoginDialog(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+
+        LoginDialog loginDialog = new LoginDialog();
+        loginDialog.show(ft, "login");
     }
 
 }
---
+++ app/src/main/java/com/saylonn/chatapp/ui/dialogs/LoginDialog.java
@@ -0,0 +1,21 @@
+package com.saylonn.chatapp.ui.dialogs;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.DialogFragment;
+
+import com.saylonn.chatapp.R;
+
+public class LoginDialog extends DialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.login_dialog, container, false);
+
+    }
+}
