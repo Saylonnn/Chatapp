3f5abfb7a338862cf5da8944d96d927113832d84
samue <samuel.raak@gmx.de>
2023-01-18 16:24:04 +0100

    add password encryption

--- app/src/main/java/com/saylonn/chatapp/ui/search/SearchRecyclerViewComponents/CustomSearchViewHolder.java
+++ app/src/main/java/com/saylonn/chatapp/ui/search/SearchRecyclerViewComponents/CustomSearchViewHolder.java
@@ -1,5 +1,6 @@
 package com.saylonn.chatapp.ui.search.SearchRecyclerViewComponents;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.saylonn.chatapp.R;
 
 public class CustomSearchViewHolder extends RecyclerView.ViewHolder{
+    private final String TAG = "CAPP";
     public TextView textUsername, textEmail;
     public Button contactButton;
 
@@ -18,6 +20,12 @@ public class CustomSearchViewHolder extends RecyclerView.ViewHolder{
             textUsername = itemView.findViewById(R.id.contact_name);
             textEmail = itemView.findViewById(R.id.contact_email);
             contactButton = itemView.findViewById(R.id.searchRcButton);
+
+            //Karol hier
+            itemView.findViewById(R.id.searchRcButton).setOnClickListener(v -> {
+                // was auch immer du tun willst
+                Log.d(TAG, "Button von " + textEmail.getText().toString() + " gedrückt.");
+            });
 
     }
 }
