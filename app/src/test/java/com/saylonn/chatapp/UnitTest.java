1d2601d3dbd67e6e455160d8c7e4892c44ebf397
Lucas <lucassommer.99@gmail.com>
2023-01-22 14:57:45 +0100

    Adding 2 Tests
    CustomSearchAdapter To getItemCount with 0 and 2 Items

---
+++ app/src/test/java/com/saylonn/chatapp/UnitTest.java
@@ -0,0 +1,60 @@
+package com.saylonn.chatapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import android.content.Context;
+
+import com.saylonn.chatapp.ui.search.SearchRecyclerViewComponents.Contact;
+import com.saylonn.chatapp.ui.search.SearchRecyclerViewComponents.CustomSearchAdapter;
+
+import java.util.ArrayList;
+
+
+public class UnitTest {
+    @Test
+    public void ContactToGetUsername(){
+        String name = "test";
+        String mail = "test@gmail.com";
+        String expected = "test";
+        Contact test = new Contact(name, mail);
+        String actual = test.getUsername();
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void ContactToGetEmail(){
+        String name = "test";
+        String mail = "test@gmail.com";
+        String expected ="test@gmail.com";
+        Contact test = new Contact(name, mail);
+        String actual = test.getEmail();
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void CustomSearchAdapterToGetItemCount0Items(){
+        ArrayList<Contact> list = new ArrayList<>();
+        Context context = null;
+        CustomSearchAdapter test = new CustomSearchAdapter(context , list);
+        String expected = "0";
+        String actual = String.valueOf(test.getItemCount());
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void CustomSearchAdapterToGetItemCount2Items(){
+        ArrayList<Contact> list = new ArrayList<>();
+        String name = "test";
+        String mail = "test@gmail.com";
+        Contact contact = new Contact(name, mail);
+        name = "tester";
+        mail = "tester@gmail.com";
+        Contact contact2 = new Contact(name, mail);
+        list.add(contact);
+        list.add(contact2);
+        Context context = null;
+        CustomSearchAdapter test = new CustomSearchAdapter(context , list);
+        String expected = "2";
+        String actual = String.valueOf(test.getItemCount());
+        assertEquals(expected, actual);
+    }
+}
